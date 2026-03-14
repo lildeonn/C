e15162b73fd9094963d47e61551aa65c8fcd4a38
lildeonn <137290375+lildeonn@users.noreply.github.com>
2026-03-14 16:34:27 +1000

    Create a.c

---
+++ a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+	char name[25];
+	printf("napishi svoyo imya:\n");
+	scanf("%20s", name);
+	printf("Pivet, %s, kak dela?\n", name);
+	return 0;
+}
