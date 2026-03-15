fea07abb176ebbcaa5b63cf43892127339686bca
lildeonn <137290375+lildeonn@users.noreply.github.com>
2026-03-15 15:01:07 +1000

    Update b.c
    ddobavlen cikl

--- b.c
+++ b.c
@@ -2,18 +2,34 @@
 
 int main(){
 	int first, second;
+	char next;
 	
-	printf("vvedite pervoe chislo: ");
-	scanf("%d", &first);
-	
-	printf("vvedite vtoroe chislo: ");
-	scanf("%d", &second);
-	
-	printf("\nsumma: %d\n", first+second);
-	printf("raznost: %d\n", first-second);
-	printf("umnozhenie: %d\n", first*second);
-	printf("delenie (celoe): %d\n", first/second);
-	printf("ostatok: %d\n", first%second);
-	
+	while(1 == 1){
+		
+		printf("vvedite pervoe chislo: ");
+		scanf("%d", &first);
+		
+		printf("vvedite vtoroe chislo: ");
+		scanf("%d", &second);
+		
+		printf("\nsumma: %d\n", first+second);
+		printf("raznost: %d\n", first-second);
+		printf("umnozhenie: %d\n", first*second);
+		printf("delenie (celoe): %d\n", first/second);
+		printf("ostatok: %d\n", first%second);
+		
+		do {
+            printf("\nprodolzhit? (y/n): ");
+            scanf(" %c", &next);  //probel pered %c chtobi iskluchit sluchaynie probeli
+
+            if (next != 'y' && next != 'Y' && next != 'n' && next != 'N') {
+                printf("neverniy otvet! poprobuyte eshe raz.\n");
+            }
+        } while (next != 'y' && next != 'Y' && next != 'n' && next != 'N');
+
+        if (next == 'n' || next == 'N') {
+            break;
+        }
+	}
 	return 0;
 }
