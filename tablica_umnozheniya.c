ce91ab3690e3465c997c17383bddc01505b85d8d
lildeonn <137290375+lildeonn@users.noreply.github.com>
2026-03-16 21:20:14 +1000

    Update tablica_umnozheniya.c
    cikl

--- tablica_umnozheniya.c
+++ tablica_umnozheniya.c
@@ -2,11 +2,23 @@
 
 int main(){
 	int chislo, i;
-	printf("vvedite chislo: ");
-	scanf("%d", &chislo);
-	for(i = 0; i < 10; i++){
-		printf("%d x %d = %d\n", chislo, i + 1, chislo * (i+1));
+	char prod;
+	while (1){
+		printf("vvedite chislo: ");
+		scanf("%d", &chislo);
+		for(i = 0; i < 10; i++){
+			printf("%d x %d = %d\n", chislo, i + 1, chislo * (i+1));
+		}
+		do{
+			printf("\nprodolzhit? (y/n) ");
+			scanf("%s", &prod);
+			if (prod != 'y' && prod != 'Y' && prod != 'n' && prod != 'N') {
+                printf("nuzhno pisat druguyu bukvu!!!!!!!\n");
+        	}
+		}while (prod != 'y' && prod != 'Y' && prod != 'n' && prod != 'N');
+		if(prod == 'n' || prod == 'N'){
+			break;
+		}
 	}
-	
 	return 0;
 }
