43610275bc7efbb858cbe3846407b96286e8cc57
lildeonn <137290375+lildeonn@users.noreply.github.com>
2026-03-21 10:47:43 +1000

    Fix scanf format and modify character handling

--- lab3.c
+++ lab3.c
@@ -4,11 +4,14 @@ int main(){
 
 	int i;
 	char str[81];
-	scanf("%80s[^\n]", str);
+	scanf("%80[^\n]", str);
 
 	for (i = 0; str[i] != '\0'; i++) {
 		if (str[i] >= 'a' && str[i] <= 'b'){
 			str[i] -= 'a' - 'A';
+		}
+		if (str[i] == '!'){
+			str[i] = '?';
 		}
 	}
 		
