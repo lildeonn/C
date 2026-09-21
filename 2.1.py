83159dc5f19d4bfc154758e4fece61f790d4c34c
lildeonn <137290375+lildeonn@users.noreply.github.com>
2026-09-21 14:45:51 +1000

    Update 2.1.py

--- 2.1.py
+++ 2.1.py
@@ -2,23 +2,18 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 
-names = ['A', 'B', 'C', 'D']
+# 2. Ввод матрицы смежности
+n = int(input("Число вершин: "))
+print("Вводи строки матрицы через пробел:")
+A = np.array([list(map(int, input().split())) for _ in range(n)])
+names = 'ABCDEFGHIJ'[:n]
 
-# 2. Матрица смежности
-A = np.array([[0,1,0,1],
-              [1,0,1,1],
-              [0,1,0,1],
-              [1,1,1,0]])
-
-# 1. Рисуем граф
+# 1. Граф
 G = nx.relabel_nodes(nx.Graph(A), dict(enumerate(names)))
-pos = {'A': (2, 1), 'B': (1, 2), 'C': (0, 1), 'D': (1, 0)}
-nx.draw(G, pos, with_labels=True, node_color='red', node_size=700)
-plt.show()
 
 # 3. Матрица инцидентности
-edges = [(i, j) for i in range(4) for j in range(i+1, 4) if A[i][j]]
-I = np.zeros((4, len(edges)), dtype=int)
+edges = [(i, j) for i in range(n) for j in range(i+1, n) if A[i][j]]
+I = np.zeros((n, len(edges)), dtype=int)
 for k, (i, j) in enumerate(edges):
     I[i][k] = 1
     I[j][k] = 1
@@ -26,13 +21,15 @@ for k, (i, j) in enumerate(edges):
     print(f'e{k+1} = {names[i]}{names[j]}')
 print('Матрица инцидентности:\n', I)
 
-# 4. Матрица Кирхгофа (через ориентированную инцидентность)
+# 4. Матрица Кирхгофа
 O = I.copy()
 for k, (i, j) in enumerate(edges):
     O[j][k] = -1
-K = O @ O.T
-print('Матрица Кирхгофа:\n', K)
+print('Матрица Кирхгофа:\n', O @ O.T)
 
 # 5. Эксцентриситет и радиус
 print('Эксцентриситеты:', nx.eccentricity(G))
 print('Радиус:', nx.radius(G))
+
+nx.draw(G, nx.circular_layout(G), with_labels=True, node_color='red', node_size=700)
+plt.show()
