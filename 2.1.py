4362bf429e0da6394096f64a8523e21f7ca66072
lildeonn <137290375+lildeonn@users.noreply.github.com>
2026-09-21 14:15:18 +1000

    Update 2.1.py

--- 2.1.py
+++ 2.1.py
@@ -1,15 +1,16 @@
 import networkx as nx
+import numpy as np
 import matplotlib.pyplot as plt
 
-G = nx.petersen_graph()
+P0 = [[0,1,1,1,0,1],
+      [1,0,1,1,0,1],
+      [1,1,0,1,0,1],
+      [0,1,0,0,0,0],
+      [1,1,1,1,0,1],
+      [1,1,1,1,1,0]]
+G = nx.DiGraph(np.array(P0))
 
-pos1 = {0: (90, 308), 6: (55, 260), 1: (121, 258), 4: (4, 233), 5: (150, 170),
-        9: (88, 142), 8: (52, 72), 3: (0, 20), 2: (100, 5), 7: (168, 25)}
-pos2 = {4: (62, 308), 3: (166, 265), 9: (121, 203), 0: (0, 172), 8: (135, 157),
-        7: (130, 108), 5: (75, 77), 6: (108, 77), 2: (166, 32), 1: (62, 7)}
-
-plt.subplot(121)
-nx.draw(G, pos1, with_labels=True, font_weight='bold')
-plt.subplot(122)
-nx.draw(G, pos2, with_labels=True, font_weight='bold')
+pos = {2: (177, 290), 4: (0, 237), 1: (342, 233), 5: (204, 115), 0: (65, 57), 3: (380, 2)}
+nx.draw(G, pos, with_labels=True, node_size=300, arrows=True)
 plt.show()
+
