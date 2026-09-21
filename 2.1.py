71b3d7557c39e06412489349174863a7175b1ef9
lildeonn <137290375+lildeonn@users.noreply.github.com>
2026-09-21 14:20:51 +1000

    Update 2.1.py

--- 2.1.py
+++ 2.1.py
@@ -2,13 +2,9 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 
-P0 = [[0,1,1,1,0],
-      [1,0,1,1,0],
-      [1,1,0,1,0],
-      [0,1,0,0,0],
-      [1,1,1,1,0]]
-G = nx.DiGraph(np.array(P0))
-
-pos = {3: (405, 304), 1: (94, 194), 2: (1, 67), 4: (349, 60), 0: (198, 1)}
-nx.draw(G, pos, with_labels=True, node_size=300, arrows=True)
+n = int(input("Число вершин: "))
+print("Вводи строки матрицы через пробел:")
+P = [list(map(int, input().split())) for _ in range(n)]
+G = nx.DiGraph(np.array(P))
+nx.draw(G, with_labels=True, node_size=300, arrows=True)
 plt.show()
